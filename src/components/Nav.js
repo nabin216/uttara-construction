42700c7bbf628d01f3f744586063a0335934af81
snehafarkya <snhafarkya@gmail.com>
2022-07-10 22:50:09 +0530

    navbar responsive

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,47 +1,56 @@
-import React from 'react'
-// import 'flowbite';
-import logo from '../assets/logo.jpeg'
-import { Navbar } from 'flowbite-react';
+import { useState } from "react"
+import "../Navbar.css"
+import logo from "../assets/logo.png"
+
 export default function Nav() {
+  const [isNavExpanded, setIsNavExpanded] = useState(false)
+
   return (
-    <div>
-      
-     <Navbar
-  fluid={true}
-  rounded={false} 
->
-  <Navbar.Brand href="https://shreeram-website.vercel.app/">
-    <img
-      src={logo} 
-      className="mr-2 h-20 sm:h-9"
-      alt="Shreeram Logo"
-    />
-    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-      Shreeram
-    </span>
-  </Navbar.Brand>
-  <Navbar.Toggle />
-  <Navbar.Collapse>
-    <Navbar.Link
-      href="/"
-      active={true}
-    >
-      Home
-    </Navbar.Link>
-    {/* <Navbar.Link href="/about">
-      About
-    </Navbar.Link> */}
-    <Navbar.Link href="/service">
-      Services
-    </Navbar.Link>
-    <Navbar.Link href="/construction">
-     Construction
-    </Navbar.Link>
-    <Navbar.Link href="/contact">
-      Contact
-    </Navbar.Link>
-  </Navbar.Collapse>
-</Navbar>
-    </div>
-  )
+    <nav className="navigation">
+        <img src={logo} alt="" height={80} width={80} />
+      <a href="https://shreeram-website.vercel.app/" className="brand-name">
+        Shreeram
+
+      </a>
+      <button
+        className="hamburger"
+        onClick={() => {
+          setIsNavExpanded(!isNavExpanded)
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="white"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+      <div
+        className={
+          isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
+        }
+      >
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/service">Services</a>
+          </li>
+          <li>
+            <a href="/construction">Construction</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
