8c216cb38d15213faae46361ddcc6db54691445f
snehafarkya <snhafarkya@gmail.com>
2022-07-18 19:16:12 +0530

    added website counter

--- src/Count.js
+++ src/Count.js
@@ -8,7 +8,7 @@ export default function Count() {
    
     <div>
      <h1 id="count"><a href="https://www.hitwebcounter.com" target="_blank">
-<img src="https://hitwebcounter.com/counter/counter.php?page=8016427&style=0020&nbdigits=5&type=page&initCount=100" title="Free Counter" Alt="web counter"   border="0" />Views</a>    
+<img src="https://hitwebcounter.com/counter/counter.php?page=8016427&style=0020&nbdigits=5&type=page&initCount=100" title="Free Counter" Alt="web counter"   border="0" /><div className="view">Views</div></a>    
 </h1>  
 </div>
   )
