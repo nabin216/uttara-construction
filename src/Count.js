4ba50c58588fcb851a9f914e399309a1341c38e8
snehafarkya <snhafarkya@gmail.com>
2022-07-18 19:27:41 +0530

    added website counter

--- src/Count.js
+++ src/Count.js
@@ -7,8 +7,8 @@ export default function Count() {
   return (
    
     <div>
-     <h1 id="count"><a href="https://www.hitwebcounter.com" target="_blank">
-<img src="https://hitwebcounter.com/counter/counter.php?page=8016427&style=0020&nbdigits=5&type=page&initCount=100" title="Free Counter" Alt="web counter"   border="0" /><div className="view">Views</div></a>    
+     <h1 id="count"><a href="https://shreeram-website.vercel.app/" target="_blank">
+<img src="https://hitwebcounter.com/counter/counter.php?page=8016427&style=0020&nbdigits=5&type=page&initCount=100"  Alt="web counter"   border="0" /><div className="view">Views</div></a>    
 </h1>  
 </div>
   )
