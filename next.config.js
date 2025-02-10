98879bc16fcb3c364b1b8cfe70d1b5e541ea53cf
Nabin <asadurzamannabin@gmail.com>
2025-02-10 10:07:48 +0530

    Initial commiwt

---
+++ next.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/uttara-construction/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/uttara-construction' : '',
+};
